54308619f5f77774da46793de9dbd3ea5f4b5db0
Varun Pal Singh <singhvarun928@gmail.com>
2026-04-04 15:56:46 +0530

    Add: design-hashset - submission-7

---
+++ Data Structures & Algorithms/design-hashset/submission-7.cpp
@@ -0,0 +1,48 @@
+class MyHashSet {
+private:
+    int n;
+    vector<list<int>> st;
+
+    int hash(int key) {
+        return key % n;
+    }
+
+public:
+    MyHashSet() {
+        n = 1e4 + 1;
+        st.resize(n, list<int>{});
+    }
+    
+    void add(int key) {
+        int idx = hash(key);
+    
+        if (!this->contains(key)) {
+            st[idx].push_back(key);
+        }
+    }
+    
+    void remove(int key) {
+        int idx = hash(key);
+        if (!this->contains(key)) return;
+
+        for (list<int>::iterator it = st[idx].begin(); it != st[idx].end(); it++) {
+            if (*it == key) {
+                st[idx].erase(it);
+                return;
+            }
+        }
+    }
+    
+    bool contains(int key) {
+        int idx = hash(key);
+        return std::find(st[idx].begin(), st[idx].end(), key) != st[idx].end();
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
