81a72b5e62cda70dae0ea0f77a6fc182e2bc9b70
Varun Pal Singh <singhvarun928@gmail.com>
2026-04-05 22:02:05 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = nums[0];
+        for (int i = 1; i < n; i++) {
+            res ^= nums[i];
+        }
+
+        return res;
+    }
+};
