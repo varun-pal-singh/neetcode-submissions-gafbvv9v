7106862e0184ba9743661234a3d68e7a7ba118fe
Varun Pal Singh <singhvarun928@gmail.com>
2026-04-16 20:47:57 +0530

    Add: reverse-integer - submission-3

---
+++ Data Structures & Algorithms/reverse-integer/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) {
+        const int MIN = -(1 << 31);
+        const int MAX = (1 << 31) - 1;
+        long long res = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            // if (res > MAX / 10 || (res == MAX / 10 && digit > MAX % 10)) {
+            //     return 0;
+            // }
+            // if (res < MIN / 10 || (res == MIN / 10 && digit < MIN % 10)) {
+            //     return 0;
+            // }
+            res = (res * 10) + digit;
+
+            if (res < MIN || res > MAX) return 0;
+        }
+        return static_cast<int>(res);
+    }
+};
