46093237968e0fd90320af60036bcbc0387d3744
Varun Pal Singh <singhvarun928@gmail.com>
2026-05-08 14:40:17 +0530

    Add: largest-rectangle-in-histogram - submission-3

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> stk; // monotonic increasing stack
+        int maxArea = 0;
+        for (int i = 0; i <= n; i++) {
+            while (!stk.empty() && (i == n || heights[stk.top()] >= heights[i])) {
+                int height = heights[stk.top()]; stk.pop();
+                int width  = stk.empty() ? i : (i - stk.top()) - 1;
+                maxArea    = std::max(maxArea, height * width);
+            }
+            stk.push(i);
+        }
+
+        return maxArea;
+    }
+};
